f99497a04c3589acd8689ea19f83d3b2f3b82ec9
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-30 02:50:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,13 @@ class Triangle
     @side_3 = side_3
   end
 
-  def legal? 
-    if side_1 && side_2 && side_3 > 0 
+  def legal?
+    if side_1 && side_2 && side_3 > 0
       true
   end
 
   def kind
-    if 
+    if
     if @side_1 == @side_2 && @side_1 == @side_3 && @side_2 == @side_3
       :equilateral
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
