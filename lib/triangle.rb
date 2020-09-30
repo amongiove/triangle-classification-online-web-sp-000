113d838d43364c4dffc093bbffc13387ae9ff20a
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-30 02:55:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,8 @@ class Triangle
   def legal?
     if side_1 > 0 && side_2 > 0 && side_3 > 0 && side_1 + side_2 > side_3 && side_2 + side_3 > side_1 && side_1 + side_3 > side_2
       true
+    else
+      false
     end
   end
 
