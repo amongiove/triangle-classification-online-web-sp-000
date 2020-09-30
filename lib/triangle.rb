91f86f92bfe181092e16da8d7352266b2ca61c3f
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-30 02:53:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def legal?
-    if side_1 && side_2 && side_3 > 0
+    if side_1 && side_2 && side_3 > 0 && side_1 + side_2 > side_3 || side_2 + side_3 > side_1 || side_1 + side_3 > side_2
       true
     end
   end
