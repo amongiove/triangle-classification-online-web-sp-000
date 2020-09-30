e1baf8667efc7f4ad70966c57259c398368116f7
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-30 02:21:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,12 +10,13 @@ class Triangle
 
   def kind(side_1, side_2, side_3)
     if side_1 == side_2 == side_3
-      equilateral
+      @kind = equilateral
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
-      isosceles
+      @kind = isosceles
     else
-      scalene
+      @kind = scalene
     end
+    @kind
   end
 
 
